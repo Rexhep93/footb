85ccdffb7865021052c0b3bd8cf78033662e0ec8
Rexhep93 <131242735+Rexhep93@users.noreply.github.com>
2026-04-09 14:21:33 +0200

    Update api.js

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 const BASE = 'https://score-app.rexhep93.workers.dev/v4';
 
-export const ALLOWED = ['PL','BL1','SA','PD','FL1','DED','PPL','ELC','BSA','CL','EC','WC'];
+export const ALLOWED = ['PL','BL1','SA','PD','FL1','DED','PPL','ELC','BSA','CL','EC','WC','CLI'];
 
 export async function getMatches(date) {
   const r = await fetch(`${BASE}/matches?dateFrom=${date}&dateTo=${date}`);
