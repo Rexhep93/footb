546e49a6e3c5fe8f12c7d96abafef143e9545280
Rexhep93 <131242735+Rexhep93@users.noreply.github.com>
2026-04-11 09:14:18 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,16 @@
+window.App = window.App || {};
+window.App.config = {
+  PDOK_BASE: 'https://api.pdok.nl/bzk/locatieserver/search/v3_1',
+  CBS_BASE: 'https://opendata.cbs.nl/ODataApi/OData',
+  CBS_DATASET: '85984NED',
+  STORAGE_KEY: 'dichtbij:address:v1',
+  GROUP_ORDER: [
+    'Bevolking',
+    'Wonen',
+    'Inkomen',
+    'Sociale zekerheid',
+    'Energie',
+    'Zorggebruik',
+    'Nabijheid voorzieningen',
+  ],
+};
